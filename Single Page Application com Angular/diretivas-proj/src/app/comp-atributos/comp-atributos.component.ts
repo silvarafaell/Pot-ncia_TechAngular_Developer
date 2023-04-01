f4511096fe3f39cea058e30bf96305c446535186
francisco rafael <francisco_rafael@hotmail.com.br>
2023-04-01 08:05:22 -0300

    NgStule

--- Single Page Application com Angular/diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
+++ Single Page Application com Angular/diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompAtributosComponent implements OnInit{
   estilo:string="disable"
+  corFundo:string="red"
 
   constructor() {}
 
